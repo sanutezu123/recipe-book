ffc7984247e4e0c675f0fb0871ead2c6b95744b6
santeuzu123 <Hrishikesh.malakar94@gmail.com>
2020-09-02 00:38:12 +0530

    remove env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyDd3sMTZVkweZaEUE3JXCEyxnnaSQXhOzk",
-    authDomain: "recipe-book-304d1.firebaseapp.com",
-    databaseURL: "https://recipe-book-304d1.firebaseio.com",
-    projectId: "recipe-book-304d1",
-    storageBucket: "recipe-book-304d1.appspot.com",
-    messagingSenderId: "973316477241",
-    appId: "1:973316477241:web:8c328a09bf04ffeb8b98e8",
-    measurementId: "G-RG11L41QFQ"
-  }
+  // production: true,
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyDd3sMTZVkweZaEUE3JXCEyxnnaSQXhOzk",
+  //   authDomain: "recipe-book-304d1.firebaseapp.com",
+  //   databaseURL: "https://recipe-book-304d1.firebaseio.com",
+  //   projectId: "recipe-book-304d1",
+  //   storageBucket: "recipe-book-304d1.appspot.com",
+  //   messagingSenderId: "973316477241",
+  //   appId: "1:973316477241:web:8c328a09bf04ffeb8b98e8",
+  //   measurementId: "G-RG11L41QFQ"
+  // }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDd3sMTZVkweZaEUE3JXCEyxnnaSQXhOzk",
-    authDomain: "recipe-book-304d1.firebaseapp.com",
-    databaseURL: "https://recipe-book-304d1.firebaseio.com",
-    projectId: "recipe-book-304d1",
-    storageBucket: "recipe-book-304d1.appspot.com",
-    messagingSenderId: "973316477241",
-    appId: "1:973316477241:web:8c328a09bf04ffeb8b98e8",
-    measurementId: "G-RG11L41QFQ"
-  }
+  // production: false,
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyDd3sMTZVkweZaEUE3JXCEyxnnaSQXhOzk",
+  //   authDomain: "recipe-book-304d1.firebaseapp.com",
+  //   databaseURL: "https://recipe-book-304d1.firebaseio.com",
+  //   projectId: "recipe-book-304d1",
+  //   storageBucket: "recipe-book-304d1.appspot.com",
+  //   messagingSenderId: "973316477241",
+  //   appId: "1:973316477241:web:8c328a09bf04ffeb8b98e8",
+  //   measurementId: "G-RG11L41QFQ"
+  // }
 };
 
 /*
