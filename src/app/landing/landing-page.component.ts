db2e0fe5fdf5b051758fc38fe387ec3562eda180
santeuzu123 <Hrishikesh.malakar94@gmail.com>
2020-09-02 00:35:58 +0530

    landing page

--- src/app/landing/landing-page.component.ts
+++ src/app/landing/landing-page.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'landing-page',
-    styleUrls: ['./landing-page.componenet.css'],
-    templateUrl: './landing-page.componenet.html'
+    styleUrls: ['./landing-page.component.css'],
+    templateUrl: './landing-page.component.html'
 })
 export class LandingPage implements OnInit {
     // tslint:disable-next-line:max-line-length
-    coverImage = 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
-    siteName = 'Food Stories';
+    siteName = 'RecipeBook';
     content = 'A place to share your recipes to the world';
-
+    quote = 'A recipe has no Soul. You as, the cook must bring the soul to it';
+    quoteAuthor = 'Thomas Keller';
     myStyle: object = {};
     myParams: object = {};
     width: number = 100;
